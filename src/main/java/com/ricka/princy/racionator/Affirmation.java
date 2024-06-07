2c13ed4d8c25bc856082015e70f6755a27f12132
hei-ricka <hei.ricka.3@gmail.com>
2024-06-07 15:45:06 +0300

    feat: operation et

--- src/main/java/com/ricka/princy/racionator/Affirmation.java
+++ src/main/java/com/ricka/princy/racionator/Affirmation.java
@@ -7,11 +7,20 @@ public class Affirmation {
     private final String contenu;
     private final TypeDAffirmation typeDAffirmation;
 
-    public Affirmation ou(Affirmation affirmation){
-        throw new Error("Not implemented");
+    public Affirmation et(Affirmation affirmation){
+        final String nouveau_contenu = combinerContenu(this, affirmation, "ou");
+
+        if(estVrai(this) && estVrai(affirmation)){
+            return new Affirmation(nouveau_contenu, TypeDAffirmation.VERITE);
+        }
+        if(estFaux(this) || estFaux(affirmation)){
+            return new Affirmation(nouveau_contenu, TypeDAffirmation.MENSONGE);
+        }
+
+        return new Affirmation(nouveau_contenu, TypeDAffirmation.AFFIRMATION);
     }
 
-    public Affirmation et(Affirmation affirmation){
+    public Affirmation ou(Affirmation affirmation){
         throw new Error("Not implemented");
     }
 
@@ -20,6 +29,26 @@ public class Affirmation {
     }
 
     public Reponse calculer(){
-        throw new Error("Not implemented");
+        return switch (this.typeDAffirmation){
+            case AFFIRMATION -> Reponse.jenesaispas;
+            case MENSONGE -> Reponse.faux;
+            case VERITE -> Reponse.vrai;
+        };
+    }
+
+    private static boolean estVrai(Affirmation affirmation){
+        return affirmation.typeDAffirmation.equals(TypeDAffirmation.VERITE);
+    }
+
+    private static boolean estJenesaispas(Affirmation affirmation){
+        return affirmation.typeDAffirmation.equals(TypeDAffirmation.AFFIRMATION);
+    }
+
+    private static boolean estFaux(Affirmation affirmation){
+        return affirmation.typeDAffirmation.equals(TypeDAffirmation.MENSONGE);
+    }
+
+    private String combinerContenu(Affirmation a,Affirmation b, String separateur){
+        return String.format("%s %s %s", a, b, separateur);
     }
 }
---
+++ src/test/java/racionator/OperationEtTest.java
@@ -0,0 +1,31 @@
+package racionator;
+
+import com.ricka.princy.racionator.Affirmation;
+import com.ricka.princy.racionator.Reponse;
+import com.ricka.princy.racionator.TypeDAffirmation;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OperationEtTest {
+    @Test
+    public void doit_etre_vraie_si_les_deux_affirmation_sont_verite(){
+        Affirmation verite = new Affirmation("Lou est beau.", TypeDAffirmation.VERITE);
+        assertEquals(verite.et(verite).calculer(), Reponse.vrai);
+    }
+
+    @Test
+    public void doit_etre_fausse_si_une_des_affirmation_est_mensonge(){
+        Affirmation verite = new Affirmation("Lou est beau.", TypeDAffirmation.VERITE);
+        Affirmation mensonge = new Affirmation("Lou est pauvre", TypeDAffirmation.MENSONGE);
+        assertEquals(verite.et(verite).et(mensonge.et(verite)).calculer(), Reponse.faux);
+    }
+
+    @Test
+    public void doit_etre_je_sais_pas_si_une_affirmation_est_inconue(){
+        Affirmation verite = new Affirmation("Lou est beau.", TypeDAffirmation.VERITE);
+        Affirmation affirmation = new Affirmation("Lou est pauvre", TypeDAffirmation.AFFIRMATION);
+        assertEquals(verite.et(affirmation).calculer(), Reponse.jenesaispas);
+        assertEquals(verite.et(affirmation.et(verite)).calculer(), Reponse.jenesaispas);
+    }
+}
